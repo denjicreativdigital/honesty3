2b8e4d669ab67be1706a4c25e0e45f957b223307
denjicreative <thewira27@gmail.com>
2025-06-11 15:53:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// toggle class active
+const navbarnav = document.querySelector('.navbar-nav');
+
+// ketika navbar hamburger menu di click
+document.querySelector('#menu').
+onclick = () => {
+    navbarnav.classList.toggle('active');
+};
+
+// menghilangkan navbar hamburger menu diluar sidebar
+const hamburger = document.querySelector('#menu');
+
+document.addEventListener('click', function(e) {
+    if (!hamburger.contains(e.target) && !navbarnav.contains(e.target)) {
+        navbarnav.classList.remove('active');
+    }
+});
